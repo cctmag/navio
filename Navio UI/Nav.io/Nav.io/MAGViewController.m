5f7920f01aea2533a5cf3439eea6321c00b5b0df
Jesse Allison <jesse@electrotap.com>
2013-09-20 20:15:17 -0500

    Added Map and table view

--- Navio UI/Nav.io/Nav.io/MAGViewController.m
+++ Navio UI/Nav.io/Nav.io/MAGViewController.m
@@ -9,6 +9,8 @@
 #import "MAGViewController.h"
 
 @interface MAGViewController ()
+- (IBAction)centerOnCCT:(UIBarButtonItem *)sender;
+@property (weak, nonatomic) IBOutlet MKMapView *cctMap;
 
 @end
 
@@ -26,4 +28,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)centerOnCCT:(UIBarButtonItem *)sender {
+    MKCoordinateRegion theRegion = self.cctMap.region;
+    NSLog(@"Map Coords %f, %f :: %f, %f", theRegion.center.latitude, theRegion.center.longitude, theRegion.span.latitudeDelta, theRegion.span.latitudeDelta);
+    
+    theRegion.center.latitude = 30.407614;
+    theRegion.center.longitude = -91.171743;
+    theRegion.span.latitudeDelta = 0.002493;
+    theRegion.span.longitudeDelta = 0.002493;
+    
+    [self.cctMap setRegion:theRegion animated:YES];
+    
+}
 @end
